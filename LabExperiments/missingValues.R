3993112e99e7de8cc41b845629b0f6386860beb9
jeslinpjames <jeslinpjames007@gmail.com>
2024-08-13 12:16:02 +0530

    commiting changes

--- LabExperiments/missingValues.R
+++ LabExperiments/missingValues.R
@@ -17,3 +17,19 @@ data$marks1[is.na(data$marks1)] <- mean(data$marks1,na.rm = TRUE)
 data$marks2[is.na(data$marks2)] <- mean(data$marks2,na.rm = TRUE)
 data$marks3[is.na(data$marks3)] <- mean(data$marks3,na.rm = TRUE)
 data
+summary(data)
+
+#install packages
+install.packages("Hmisc")
+library(Hmisc)
+impute(data$marks2,median)
+
+data <- rnorm(500)
+data[1:10] <- c(46,9,15,-90,
+                42,50,-82,74,61,-32)
+
+
+data <- rnorm(500)
+data[1:10] <- c(46,9,15,-90,
+                42,50,-82,74,61,-32) 
+boxplot(data)
